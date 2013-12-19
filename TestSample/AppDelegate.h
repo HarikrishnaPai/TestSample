74ab194d1fb8fe4c464e6e08d94d4721286b5e62
Harikrishna Pai <harikrishnapair@gmail.com>
2013-12-19 19:44:36 +0530

    Test Commit

---
+++ TestSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSample
+//
+//  Created by Hari on 19/12/13.
+//  Copyright (c) 2013 GTL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
